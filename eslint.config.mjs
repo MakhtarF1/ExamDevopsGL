908208fbc389958ce28f8e5a5ade5fee151b4fcb
MakhtarF1 <fallmakh026@gmail.com>
2025-03-07 11:50:58 +0000

    mis à jour des package globales dans nodes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,26 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import parser from '@typescript-eslint/parser'; // Assure-toi d'importer le parser TypeScript
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const parser = require('@typescript-eslint/parser');
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+module.exports = [
   {
-    files: ['**/*.{js,mjs,cjs,ts,tsx}'], // Inclut les fichiers TypeScript .ts et .tsx
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
     languageOptions: {
       globals: {
-        ...globals.browser,  // Utilise les globals du navigateur si c'est le cas
+        ...globals.browser,  // Si tu veux des globals de navigateur
       },
-      parser: parser,  // Spécifie que le parser est pour TypeScript
+      parser: parser,  // Utiliser le parser pour TypeScript
       parserOptions: {
-        ecmaVersion: 2020,  // Version ECMAScript, ajuste selon ton projet
-        sourceType: 'module',  // Si tu utilises les modules ES
+        ecmaVersion: 2020,  // Choisir la version ECMAScript selon ton projet
+        sourceType: 'module',  // Si tu utilises des modules ES
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint,  // Plugin TypeScript pour ESLint
+      '@typescript-eslint': tseslint,  // Ajoute le plugin TypeScript pour ESLint
     },
   },
-  pluginJs.configs.recommended,  // Configuration JavaScript recommandée
-  ...tseslint.configs.recommended,  // Configuration TypeScript recommandée
+  pluginJs.configs.recommended,  // Configuration par défaut pour JavaScript
+  ...tseslint.configs.recommended,  // Configuration recommandée pour TypeScript
 ];
